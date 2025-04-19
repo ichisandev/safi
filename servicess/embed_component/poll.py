07b233002ef9751702855d36a1ae782b8f7bf043
Muhammad Nur Ichsan <muhammad.ichsan@dataon.com>
2025-04-19 09:47:02 +0700

    update: add poll capabilities

--- servicess/embed_component/poll.py
+++ servicess/embed_component/poll.py
@@ -1,41 +1,29 @@
 import pydantic
 from pydantic_ai import Agent
-# import discord
-import traceback
 
 from ..chatbot import memory
 from utils import config
 
 
-class PollChoice(pydantic.BaseModel):
-    choice: str
-    choice_emoji: str = pydantic.Field(description="hanya satu karakter emoji")
-
-
 class PollRequest(pydantic.BaseModel):
     poll_question: str = pydantic.Field(description="buat pertanyaan kurang dari 250 karakter")
-    poll_choice: list[PollChoice] = pydantic.Field(description="minimal 2 pilihan, maksimal 10 pilihan")
+    poll_choice: list[str] = pydantic.Field(description="minimal 2 pilihan, maksimal 10 pilihan")
 
 
 agent = Agent(
     model=config.GENAI_MODEL,
     result_type=PollRequest,
-    system_prompt=config.SYSTEM_INSTRUCTION + "buat poll dari permintaan terakhir",
+    system_prompt=config.SYSTEM_INSTRUCTION + "\nbuat poll dari permintaan terakhir",
 )
 
-
-def parse_poll(response):
-    title = "📊 " + response.data.poll_question
-    description = "\n".join(f"{choice.choice_emoji} {choice.choice}" for choice in response.data.poll_choice)
-    embed = discord.Embed(title=title, description=description, color=discord.Color.blue())
-    return embed
-
-
-async def main(message):
-    prompt = memory.get_chat_history(str(message.channel.id))
+async def main(update, context):
+    prompt = memory.get_chat_history(str(update.message.chat.id))
     response = await agent.run(prompt)
-    embed = parse_poll(response)
-    poll_message = await message.channel.send(embed=embed)
-    for choice in response.data.poll_choice:
-        await poll_message.add_reaction(choice.choice_emoji)
+    await context.bot.send_poll(
+        chat_id=update.message.chat.id,
+        question=response.data.poll_question,
+        options=response.data.poll_choice,
+        is_anonymous=False
+    )
+    memory.store_message(chatroom_id=str(update.message.chat.id), message=f"{config.BOT_NAME}: *sending poll, question={response.data.poll_question}, options={response.data.poll_choice}")
     return
